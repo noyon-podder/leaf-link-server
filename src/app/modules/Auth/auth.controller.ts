102ab08565b55cf34123c35522a4d79d28594b74
noyon-podder <noyon.podder7@gmail.com>
2024-09-29 11:05:53 +0600

    login controller completed

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -4,6 +4,7 @@ import sendResponse from '../../utils/sendResponse'
 import { AuthServices } from './auth.services'
 import config from '../../config'
 
+// register a new user with access token and refresh token
 const registerUser = catchAsync(async (req, res) => {
   const result = await AuthServices.registerUser(req.body)
 
@@ -25,6 +26,29 @@ const registerUser = catchAsync(async (req, res) => {
   })
 })
 
+// login user with  email and password and set token in cookie
+const loginUser = catchAsync(async (req, res) => {
+  const result = await AuthServices.loginUser(req.body)
+
+  const { refreshToken, accessToken } = result
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  })
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User logged in successfully!',
+    data: {
+      accessToken,
+      refreshToken,
+    },
+  })
+})
+
 export const AuthControllers = {
   registerUser,
+  loginUser,
 }
